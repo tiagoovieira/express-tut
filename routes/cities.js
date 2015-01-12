5956bd9fc977f0c843e797a61cce3fa91ca3009e
Tiago Vieira <tiagoovieira@gmail.com>
2015-01-12 17:20:11 +0000

    Fix for heroku prod

--- routes/cities.js
+++ routes/cities.js
@@ -1,61 +1,60 @@
 var express = require('express');
+
 var bodyParser = require('body-parser');
-var urlencoded = bodyParser.urlencoded({ extended: false });
+var urlencode = bodyParser.urlencoded({ extended: false });
 
-// Redis Connection 
+// Redis connection
 var redis = require('redis');
-if (process.env.REDISTOGO_URL) 
-{
-   	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-	var client = require("redis").createClient(rtg.port, rtg.hostname);
-
-	redis.auth(rtg.auth.split(":")[1]);
-} 
-else 
-{
-	var client = redis.createClient();
-	client.select((process.env.NODE_ENV || 'development').length);
-
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var client = redis.createClient(rtg.port, rtg.hostname);
+  client.auth(rtg.auth.split(":")[1]);
+} else {
+  var client = redis.createClient();
+  client.select((process.env.NODE_ENV || 'development').length);
 }
 // End Redis Connection
 
 var router = express.Router();
 
 router.route('/')
-	.get(
-		function(req, res) {
-			client.hkeys('cities', function(error, names){
-			res.json(names);
-		});
-	}) 
-	.post(urlencoded, function(req, res){
-		var newCity = req.body;
-		if(!newCity.name || !newCity.description) {
-			res.sendStatus(400);
-			return false;
-		}
+  .get(function(request, response){
+    client.hkeys('cities', function(error, names) {
+      if(error) throw error;
 
-		client.hset('cities', newCity.name, newCity.description, function(error){
-			if(error) throw error; 
-			res.status(201).json(newCity.name);
-		});
-	});
+      response.json(names);
+    });
+  })
+
+  .post(urlencode, function(request, response){
+    var newCity = request.body;
+    if(!newCity.name || !newCity.description){
+      response.sendStatus(400);
+      return false;
+    }
+    client.hset('cities', newCity.name, newCity.description, function(error) {
+      if(error) throw error;
+
+      response.status(201).json(newCity.name);
+    });
+  });
+
 
 router.route('/:name')
-	.get(function(req, res){
-		client.hget('cities', req.params.name, function(error, description){
-			res.render('show.ejs', 
-				{ city: 
-					{ name: req.params.name, description: description } 
-				});
-		});
-	})
-	.delete(function(req, res){
-		client.hdel('cities', req.params.name, function(error){
-			if(error) throw error;
-			res.sendStatus(204);
-		});
-	});
+  .delete(function(request, response) {
+    client.hdel('cities', request.params.name, function(error) {
+      if(error) throw error;
+      response.sendStatus(204);
+    });
+  })
 
+  .get(function(request, response) {
+    client.hget('cities', request.params.name, function(error, description) {
+      response.render('show.ejs',
+                      { city:
+                        { name: request.params.name, description: description }
+                      });
+    });
+  });
 
 module.exports = router;
