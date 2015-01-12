38421261750817ba6fc5ed8b9750c1c68d1b8752
Tiago Vieira <tiagoovieira@gmail.com>
2015-01-12 15:57:28 +0000

    No select for production

--- app.js
+++ app.js
@@ -18,9 +18,8 @@ if (process.env.REDISTOGO_URL)
 else 
 {
 	var client = redis.createClient();
+	client.select((process.env.NODE_ENV || 'development').length);
 }
-
-client.select((process.env.NODE_ENV || 'development').length);
 // End Redis Connection
 
 app.get('/cities', function(req, res) {
